f77961eca3b2dd8e2f8eb9a9e6af1d00470d963a
Pavel <Pavel.Vlasov@nasdanika.org>
2024-08-28 08:43:14 -0400

    2024.8.0

--- generator/src/test/java/org/nasdanika/models/bank/generator/tests/TestBankModelDocGen.java
+++ generator/src/test/java/org/nasdanika/models/bank/generator/tests/TestBankModelDocGen.java
@@ -25,7 +25,7 @@ import org.nasdanika.html.model.app.Action;
 import org.nasdanika.html.model.app.gen.AppSiteGenerator;
 import org.nasdanika.models.bank.BankPackage;
 import org.nasdanika.models.bank.processors.ecore.EcoreGenBankProcessorsFactory;
-import org.nasdanika.models.ecore.graph.processors.EcoreActionGenerator;
+import org.nasdanika.models.ecore.graph.processors.EcoreHtmlAppGenerator;
 import org.nasdanika.models.ecore.graph.processors.EcoreNodeProcessorFactory;
 import org.nasdanika.ncore.NcorePackage;
 
@@ -67,12 +67,12 @@ public class TestBankModelDocGen {
 				Map.entry(NcorePackage.eINSTANCE, URI.createURI("https://ncore.models.nasdanika.org/"))	
 			);
 			
-		EcoreActionGenerator eCoreActionGenerator = new EcoreActionGenerator(
+		EcoreHtmlAppGenerator eCoreHtmlAppGenerator = new EcoreHtmlAppGenerator(
 				BankPackage.eINSTANCE, 
 				packageURIMap, 
 				ecoreNodeProcessorFactory);
 		
-		eCoreActionGenerator.generateActionModel(diagnosticConsumer, output, progressMonitor);
+		eCoreHtmlAppGenerator.generateHtmlAppModel(diagnosticConsumer, output, progressMonitor);
 				
 		String rootActionResource = "actions.yml";
 		URI rootActionURI = URI.createFileURI(new File(rootActionResource).getAbsolutePath());//.appendFragment("/");
